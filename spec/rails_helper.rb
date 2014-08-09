100dd0757e5dca4a15ff66afb707b80fd7a5d0b1
Masha Rikhter <masha.rikhter@gmail.com>
2014-08-09 17:01:19 -0400

    Add rspec helper for controller responses

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,8 +7,20 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 
+module HandleExceptionsInSpecs
+  def process(*)
+    super
+  rescue ActiveRecord::RecordNotFound
+    @response.status = 404
+  rescue ActionController::UnknownFormat
+    @response.status = 406
+  end
+end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+
+  config.include(HandleExceptionsInSpecs, type: :controller)
 end
