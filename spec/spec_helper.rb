6e018710e50a4abff0465e54a5426dbf9bd5981f
Masha Rikhter <masha.rikhter@gmail.com>
2014-08-09 16:59:36 -0400

    Install rspec and remove test folder

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+RSpec.configure do |config|
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+end
